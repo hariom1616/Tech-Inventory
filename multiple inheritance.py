abbb64526091392cb77038c5cf6954ee4e5c9603
Kartikey Gupta <kartikeyf12@gmail.com>
2024-10-30 12:06:30 +0530

    Multiple Inheritance in Python: Implementing Parent-Child Class Relationship with Improvements

--- multiple inheritance.py
+++ multiple inheritance.py
@@ -1,15 +1,30 @@
-class father:
-    def fdisplay(self):
-        print("Father")
-class mother:
-    def mdisplay(self):
-        print("Mother")
-class child(father,mother):
-    def cdisplay(self):
-        print("Child")
-c=child()
-c.fdisplay()
-c.mdisplay()
-c.cdisplay()
-#in java there is no multiple inheritance we perform this multiple inheritance using interface..
+class Father:
+    def __init__(self):
+        print("Father Constructor Called")
 
+    def display(self):
+        print("This is the Father class")
+
+class Mother:
+    def __init__(self):
+        print("Mother Constructor Called")
+
+    def display(self):
+        print("This is the Mother class")
+
+class Child(Father, Mother):
+    def __init__(self):
+        # Call both parent constructors
+        Father.__init__(self)
+        Mother.__init__(self)
+        print("Child Constructor Called")
+
+    def display(self):
+        # Override to add unique child display
+        print("This is the Child class")
+
+# Creating an instance of the Child class
+child_instance = Child()
+child_instance.display()  # Child display method
+Father.display(child_instance)  # Access Father's display
+Mother.display(child_instance)  # Access Mother's display
