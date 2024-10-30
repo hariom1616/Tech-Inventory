4915633a8e3c5bc8392b6d9f871102ae4c02f0e4
Kartikey Gupta <kartikeyf12@gmail.com>
2024-10-30 18:52:23 +0530

    Implementing Method Overloading in Python using Default Parameters

--- Method Overloading.py
+++ Method Overloading.py
@@ -6,11 +6,11 @@ class Overloading:
             print("One parameter:", a)
         else:
             print("No parameters")
-            
+
 # Create an instance of the class
 obj = Overloading()
 
-# Call the display method with varying number of parameters
+# Call the display method with varying numbers of parameters
 obj.display()        # No parameters
 obj.display(10)      # One parameter
 obj.display(10, 20)  # Two parameters
